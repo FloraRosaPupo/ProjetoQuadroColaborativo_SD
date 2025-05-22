cb8808a4546071cefeda770ea54ba2585325b04f
JonatanHSilva <jonatan.silva@ufv.br>
2025-05-22 19:30:16 -0300

    adding core integration + changes to run.py

--- run.py
+++ run.py
@@ -2,8 +2,12 @@ from client.ui.login_screen import LoginScreen
 from PySide6.QtWidgets import QApplication
 import sys
 
+from ws_client import start_ws_client  # 👈 adiciona essa linha aqui
+
 if __name__ == "__main__":
+    start_ws_client()  # 👈 e chama aqui antes de abrir a interface
+
     app = QApplication(sys.argv)
-    login = LoginScreen()  # ✅ Corrigido aqui
+    login = LoginScreen()
     login.show()
     sys.exit(app.exec())
